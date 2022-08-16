218dfaeb4cb2686508ffe7eae51b02aa6b40ebb4
Holly Jones <holly@baysconsulting.co.uk>
2022-08-16 14:23:33 +0100

    Corrected type

--- bin/run-all.ps1
+++ bin/run-all.ps1
@@ -1,7 +1,7 @@
 $env:PYTHONPATH="$PWD/src"
 
 echo "Generating fake data..."
-jupyter nbconvert --to notebook --execute notebooks/01-fake-data-generation.ipynb.ipynb --log-level=WARN --stdout > $null
+jupyter nbconvert --to notebook --execute notebooks/01-fake-data-generation.ipynb --log-level=WARN --stdout > $null
 echo "Fake data saved to output directory"
 
 echo "Preparing data for modelling..."
